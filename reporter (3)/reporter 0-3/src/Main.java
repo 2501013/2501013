66f7e87f1a66651edc59b90062b57cb7f9ae2c38
SE10 <kminskstar@gmail.com>
2025-09-19 00:28:35 +0900

    3주차 과제제출

---
+++ reporter (3)/reporter 0-3/src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+int name = "홍길동";
+double moni = 60.96f;
+float clk = 2.8f;
+int hdd = 500;
+String etc = "방문설치";
+
+
+
+    }
+}
