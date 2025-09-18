753b2899c8b9756765e1f8ff1d1172d90ae305d5
SE10 <kminskstar@gmail.com>
2025-09-18 09:36:51 +0900

    3주차 수업자료

---
+++ week 03/sample-01/src/Main.java
@@ -0,0 +1,33 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        byte a;
+        short b;
+        int c;
+        long d;
+
+        float f;
+        double k;
+
+        char ch;
+        final float PI = 3.141592f;  //Symbolic constant(기호적 상수)
+
+        PI = 3.14f;
+
+        a =(byte) 128;
+        b =(short) 32768;
+        c =(int) 3l;
+        d = 3;
+
+        f = 31.4f;
+        k = 34.1;
+
+        ch = 'A';
+        ch = "A";
+
+        String name = "A";
+        String name = new String("test");
+
+    }
+}
---
+++ week 03/sample-04/src/Main.java
@@ -0,0 +1,19 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        String name = "홍길동";
+        int age = 21;
+        float eye = 1.2f;
+        String blood = "O";
+
+
+
+        System.out.printf("이름 : %s\n", name);
+        System.out.printf("나이 : %2d\n", 21);
+        System.out.printf("시력 : %3.1f\n", 1.2);
+        System.out.printf("혈액형 : %s\n", blood);
+    }
+}
