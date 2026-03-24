41d79bc593c6abcefbdeb322b06ed9bc66f97e73
2501013 <kminskstar@gmail.com>
2026-03-24 23:47:51 +0900

    Delete week03 - report directory

--- week03 - report/03 -report/src/Main.java
+++
@@ -1,6 +0,0 @@
-//TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
-// 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
-void main() {
-    Student lee = new Student("김소민", 2501013, "소프트웨어융합과", 2006);
-    lee.printInfo();
-}
